585b295ea6910cceac3cca89fa2716e3c3c20efa
max <maksim@cs.fsu.edu>
2020-04-15 23:27:16 -0400

    add tokenizer; load dictionary file

--- app/src/main/java/com/scnr/MLClient.kt
+++ app/src/main/java/com/scnr/MLClient.kt
@@ -5,10 +5,12 @@ import android.content.res.AssetManager
 import android.util.Log
 import androidx.annotation.WorkerThread
 import org.tensorflow.lite.Interpreter
-import java.io.File
+import java.io.BufferedReader
 import java.io.FileInputStream
+import java.io.InputStreamReader
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
+import kotlin.streams.toList
 
 
 class MLClient(private val context: Context) {
@@ -16,34 +18,35 @@ class MLClient(private val context: Context) {
         it.setNumThreads(8)
     }
 
-    private val modelFileBuffer by lazy {
-        loadModelFile(context.assets).also {
-            Log.d(TAG, "Model loaded")
-        }
-    }
+    private val modelFileBuffer = loadModelFile(context.assets)
 
-    val interpreter by lazy {
-        Interpreter(modelFileBuffer, options)
-    }
+    val interpreter = Interpreter(modelFileBuffer, options)
+
+    val dict = loadDictionaryFile(context.assets)
 
     companion object {
         val MODEL_PATH = "bert/model.tflite"
+        val DIC_PATH = "bert/vocab.txt"
         val TAG = "scnr.MLClient"
 
         @WorkerThread
-        fun loadModelFile(assetManager: AssetManager): MappedByteBuffer {
-            Log.d(TAG, "assets: ${assetManager.list("")!!.joinToString { it }}")
-            assetManager.openFd(MODEL_PATH).use { fileDescriptor ->
-                FileInputStream(fileDescriptor.fileDescriptor).use { inputStream ->
-                    return inputStream.channel.map(
+        private fun loadModelFile(assetManager: AssetManager): MappedByteBuffer =
+            assetManager.openFd(MODEL_PATH).use { fd ->
+                FileInputStream(fd.fileDescriptor).use { ist ->
+                    return ist.channel.map(
                         FileChannel.MapMode.READ_ONLY,
-                        fileDescriptor.startOffset,
-                        fileDescriptor.declaredLength
+                        fd.startOffset,
+                        fd.declaredLength
                     )
                 }
             }
-        }
+
+        @WorkerThread
+        private fun loadDictionaryFile(assetManager: AssetManager): Map<String, Int> =
+            assetManager.open(DIC_PATH).use { fd ->
+                BufferedReader(InputStreamReader(fd)).use { reader ->
+                    return reader.lines().toList().mapIndexed { index, s -> Pair(index, s) }.associateBy({it.second}, {it.first})
+                }
+            }
     }
-
-
 }
